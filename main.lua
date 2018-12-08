0ec035420a8f49639a6b35ad999c8560a10a6638
DanielSchuette <d.schuette@online.de>
2018-12-08 16:24:30 +0100

    add main.lua skeleton

---
+++ main.lua
@@ -0,0 +1,20 @@
+--[[
+    A simple Snake game
+    The code comes from CS50's Twitch live stream:
+    https://www.youtube.com/watch?v=ld_xcXdRez4
+]]
+
+function love.load()
+end
+
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    end
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+end
